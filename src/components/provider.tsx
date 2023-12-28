f1577634cf6778121f0a92c1ee3b1491da8fbe87
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-12-28 21:06:16 +0530

    redirect if not auth

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode, useEffect } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
+import { useStore } from "@/store/store";
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(
     new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } }),
   );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <AuthWrapper>{children}</AuthWrapper>
+    </QueryClientProvider>
+  );
 }
 
 export default Providers;
+
+const AuthWrapper = ({ children }: { children: ReactNode }) => {
+  const { isLoggedIn } = useStore();
+  const router = useRouter();
+  const pathname = usePathname();
+  const isAuthRoute = pathname.includes("/auth");
+
+  useEffect(() => {
+    if (!isLoggedIn() && !isAuthRoute) {
+      router.replace("/auth");
+    }
+  }, [isLoggedIn, router]);
+
+  return children;
+};
