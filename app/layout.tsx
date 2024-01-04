4a760a7ec30371e64d485a3d08e652835c946d9d
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2024-01-04 22:16:07 +0530

    Updated meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./custom-progress-bar.css";
@@ -34,34 +34,67 @@ export default function RootLayout({
         <meta name="theme-color" content="#035879" />
 
         <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/touch-icon-ipad.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+        />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <link rel="shortcut icon" href="/favicon.png" />
 
         <meta name="twitter:card" content="Brdsai Wingman" />
         <meta name="twitter:url" content="https://wingman.brdsai.com" />
         <meta name="twitter:title" content="Brdsai Wingman" />
         <meta name="twitter:description" content="---" />
-        <meta name="twitter:image" content="https://wingman.brdsai.com/logo-base.png" />
+        <meta
+          name="twitter:image"
+          content="https://wingman.brdsai.com/logo-base.png"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Brdsai Wingman" />
         <meta property="og:description" content="" />
         <meta property="og:site_name" content="Brdsai Wingman" />
         <meta property="og:url" content="https://wingman.brdsai.com" />
-        <meta property="og:image" content="https://wingman.brdsai.com/logo-base.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta
+          property="og:image"
+          content="https://wingman.brdsai.com/logo-base.png"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </head>
       <body className={inter.className}>
         <NextTopLoader color="#035879" showSpinner={false} />
-        <Provider>
-          {children}
-        </Provider>
+        <Provider>{children}</Provider>
       </body>
       <Toaster />
     </html>
